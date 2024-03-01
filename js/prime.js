8f398551c5504bdaf32458fc79b5bde2511c57c9
jd-HW <johannes.dereuddre@student.howest.be>
2024-03-01 20:32:53 +0100

    Add button identifier event function

--- js/prime.js
+++ js/prime.js
@@ -4,17 +4,54 @@ document.addEventListener("DOMContentLoaded", initialize);
 
 function initialize() {
     assignRandomNumber();
+    
+    const btnOne = document.getElementById("btn-5");
+    const btnTwo = document.getElementById("btn-3");
+    const btnTest = document.getElementById("btn-20"); 
 
-    const divOne = document.getElementById("btn-2");
-    const divTwo = document.getElementById("btn-3");
-    divOne.addEventListener("click", changeBackgroundCorrect);
-    divTwo.addEventListener("click", changeBackgroundWrong);
+    // divOne.addEventListener("click", changeBackgroundCorrect(divOne)); // How pass arguments into methods without invoking instantly???
+    
+    
+    btnOne.addEventListener("click", changeBackgroundCorrect); 
+    btnTwo.addEventListener("click", changeBackgroundWrong);
 
-    console.log(getValue(divOne));
+    const testie = btnOne.innerHTML;
 
-    console.log(isPrime(47));
+    const buttons = document.querySelectorAll("button");
+
+
+    //Button.addEventListener("click", passIntoEvtListener.bind(someString));
+
+    buttons.forEach(button => button.addEventListener("click", whatevs)); //How doe this not wait for click??
+
+    function whatevs(event){
+
+        // this.innerHTML
+
+        // alert(`Got clicked! my nr is: ${button.id}`);
+        console.log(event);
+        const btnValue = event.target.innerHTML;
+
+        if(isPrime(+btnValue)){
+            changeBackgroundCorrect(event.target);
+        }
+        else changeBackgroundWrong(event.target);
+        
+    }
+
+    // function whatevs(){
+    //     alert(`Got clicked! my nr is: ${button.id}`);
+    // }
+
+    console.log(buttons);
+
+    console.log(getClickName(btnTwo));
 
     getId();
+    console.log(getPlayerSelection());
+
+    listNumberofPrimes();
+
 }
 
 
--- js/primefunctions.js
+++ js/primefunctions.js
@@ -10,37 +10,29 @@ function assignRandomNumber(){
     
     for (let i = 1; i <= 20; i++) {
         
-        document.getElementById(`btn-${i}`).innerText = generateRandom();
-        
+        document.getElementById(`btn-${i}`).innerText = generateRandom();        
     }
 }
 
-
 //CHANGE BACKGROUND
-function changeBackgroundCorrect(){
+function changeBackgroundCorrect(element){
     const bgColor = "green";
     const borderStyle = "5px solid lightgreen";
+    // const btnEl = document.getElementById(`btn-1`);
 
-    const divEl = document.getElementById(`btn-1`);
-
-    divEl.style.backgroundColor = bgColor;
-    divEl.style.border = borderStyle;
+    element.style.backgroundColor = bgColor;
+    element.style.border = borderStyle;
+    element.disabled = true;
 }
 
-function changeBackgroundWrong(){
+function changeBackgroundWrong(element){
     const bgColor = "red";
     const borderStyle = "5px solid pink";
+    // const btnEl = document.getElementById(`btn-2`);
 
-    const divEl = document.getElementById(`btn-1`);
-
-    divEl.style.backgroundColor = bgColor;
-    divEl.style.border = borderStyle;
-}
-
-//GET INNER TEXT BASED ON ID
-function getValue(element){
-    return element.innerText;
-    
+    element.style.backgroundColor = bgColor;
+    element.style.border = borderStyle;
+    element.disabled = true;
 }
 
 //PRIME NUMBER BOOL
@@ -58,16 +50,76 @@ function isPrime(number) {
     return true;
   }
 
+  //CHECK AND LIST NUMBER OF PRIMES
+
+  function listNumberofPrimes() {
+
+    let primeCount = 0;
+
+  for (let i = 1; i <= 20; i++) {
+        
+    const current = document.getElementById(`btn-${i}`)
+    const number = current.innerText;
+    const checkifPrime = isPrime(number);
+    
+    if(checkifPrime){
+        console.log(`Prime ${number}`);
+        primeCount++;
+    }
+    else console.log(`Nope ${number}`);
+    }
+
+    const newHead = document.querySelector("h2");
+    newHead.innerText = `Number of primes to find: ${primeCount}`;
+    
+}
 
   //TRASH
-  function getId(){
 
-    const buttonPressed = e => {
-      console.log(e.target.id);  // Get ID of Clicked Element
-    }
+  function getClickName(object){
+    return object.id
+  }
+
+  function getId(){
     
+
+  for (let i = 1; i <= 20; i++) {
+    const item = document.getElementById(`btn-${i}`);
+    item.addEventListener("click", item => {console.log(item.target.id)}); // How does this log return a value but function below is undefined??
+        }
+  }
+
+  function getPlayerSelection(){
+    let choice = "";
+
     for (let i = 1; i <= 20; i++) {
+      const item = document.getElementById(`btn-${i}`);
+      item.addEventListener("click", item => {choice = item.target.id}); // How does this log return a value but function below is undefined??
+          }
+    return choice;
+    }
+
+//   function getIdNmber(){
+
+//     let idValue = "";
+
+//     for (let i = 1; i <= 20; i++) {
+//       let item = document.getElementById(`btn-${i}`);
+//       item.addEventListener("click", item => { idValue = item.target.id});
+//       return item;
+//           }
+
+//     }
+
+
+//   function getId(){
+
+//     const buttonPressed = e => {
+//       console.log(e.target.id); // Get ID of Clicked Element
+//     }
+    
+//     for (let i = 1; i <= 20; i++) {
             
-        document.getElementById(`btn-${i}`).addEventListener("click", buttonPressed);
-    }
-    }
+//         document.getElementById(`btn-${i}`).addEventListener("click", buttonPressed);
+//     }
+//     }
